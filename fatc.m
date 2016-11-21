aa10fa3e9d36549129e45a8b7159695f27f5b3c5
Geovane Pereira <geeo.sp@gmail.com>
2016-11-21 01:24:28 -0300

    ponto de partida

--- fatc.m
+++ fatc.m
@@ -1,28 +1,67 @@
-clc;
-mdata =(importdata('abalone.data'));
-data = mdata.data;
-classes = mdata.textdata;
-x = mean(data)
-countc = size(data, 2);
-countl = size(data, 1);
-for i=1:countc
+clc;%limpa o console
+mdata =(importdata('abalone.data'));%le do arquivo
+data = mdata.data; % importa os dados, aqui eles n tem informaçao da lcasse, o matlab ta ignorando as letras
+classes = mdata.textdata;% aqqui o vetor de classes, ta consistente com a tava
+countc = size(data, 2); %numero de colunas
+countl = size(data, 1);%numero de linhas
+for i=1:countc% pra cada atributo
     c = data(:,i);
-    s = sum (c);
-    data(:, i) = c*(1/s);
+    s = sum (c);% calcule a soma
+    data(:, i) = c*(1/s);%normaliza a coluna
     
 end
-sum(data)
-ndata = zeros(countl,1);
-for i=1:countl
-   if(strcmp(classes{i},'F'))
-       ndata(i) = 1;
-   elseif(strcmp(classes{i},'M'))
-        ndata(i) = 2;
-   elseif(strcmp(classes{i},'F'))
-               ndata(i) = 3;
-   end
+sum(data) %so confirmando que ta tudo normalizado
+ndata = zeros(countl,1); % nova matriz, é a data mais as informacoes de class
+% matrizes de dados separados por classe
+F = []
+M = []
+I = []
+
+for i=1:countl % preenche ndata, F, M, I com os exemplos. Enfim, a ndata sao os dados preparados para analise -- ponto de partida
    ndata(i, 2:countc+1) = data(i,1:countc);
+
+    if(strcmp(classes{i},'F'))
+       ndata(i,1) = 1;
+     
+    F=[F;ndata(i,:)];
+   elseif(strcmp(classes{i},'M'))
+        ndata(i,1) = 2;
+        
+    M=[M;ndata(i,:)];
+   elseif(strcmp(classes{i},'I'))
+        ndata(i,1) = 3;
+
+    I=[I;ndata(i,:)];
+    end
 end
+sum(F)
+sum(M)
+sum(I)
+countF = size(F,1)
+
+countM = size(M,1)
+
+countI = size(I,1)
+
+
+countF+countI+countM
+%sum(ndata)
+%so confirmando que ta tudo ok, a soma bateu, e os dados tao normalizados
+%no geral.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
